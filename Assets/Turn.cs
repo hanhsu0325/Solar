908ef987f64c902252bad93a82eeee874cbd9d85
hanhsu032596 <43651161+hanhsu032596@users.noreply.github.com>
2018-10-09 08:32:39 +0800

    Initial commit

---
+++ Assets/Turn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turn : MonoBehaviour {
+
+    public float y;
+
+
+    void Update()
+    {
+        Transform transform = GetComponent<Transform>();
+        Vector3 angle = new Vector3(0, y + Time.deltaTime, 0);
+        transform.Rotate(angle);
+    }
+}
